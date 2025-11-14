1da774bd31de78f88769f2b4d6026ff50187eedc
sameer.s.k <sameerskharad@gmail.com>
2025-11-14 16:53:37 +0530

    Production build working locally

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import supabase from "../supabaseClient.js";
+import supabase from "../supabaseClient";
 
 const ContactForm = () => {
   const [loading, setLoading] = useState(false);
