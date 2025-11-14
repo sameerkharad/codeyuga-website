ddabca99330edbc6327a996badf39b4c55992b98
sameer.s.k <sameerskharad@gmail.com>
2025-11-14 16:43:12 +0530

    updated

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,5 +4,7 @@ const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY
 );
+console.log("Vercel URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("Vercel KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 export default supabase;
